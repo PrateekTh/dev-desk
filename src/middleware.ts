1a2971e8cbce61356107ee2b9f64604f222899f0
PrateekTh <prateek33thakur@gmail.com>
2024-11-05 00:20:53 +0530

    Adding Middleware and Sign In route

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,16 @@ export { default } from 'next-auth/middleware'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-
-    const token = await getToken({req: request});
+    const secret = process.env.NEXTAUTH_SECRET
+    const token = await getToken({
+        req: request,
+        secret:secret,
+        cookieName: "next-auth.session-token"
+    });
     const url = request.nextUrl;
 
-    if(token && (
+    console.log(token)
+    if( token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/sign-up') ||
         url.pathname.startsWith('/verify') ||
@@ -19,8 +24,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
 
-    return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more
@@ -28,7 +36,7 @@ export const config = {
   matcher: [
     '/sign-in',
     '/sign-up',
-    '/',
+
     '/verify/:path*',
     '/dashboard/:path*'
   ]
